f0baa81e8c89b49775e32c7d89f3c071281b287f
Jhonny1122 <113983932+Jhonny1122@users.noreply.github.com>
2022-10-10 20:11:57 +0200

    Update ej9am.php

---
+++ ArraysMultidumensionales/ej9am.php
@@ -0,0 +1,27 @@
+<?php
+// Matriz traspuesta, se cambia las filas a columnas
+/* 
+ejemplo: 
+1	3	4
+
+2	0	2
+
+matriz traspuesta:
+
+1	2
+
+3	0
+
+4	2
+
+
+*/
+// Valores de la Matriz 1
+$matriz1 = array(
+array(2,0,1,5),
+array(3,0,0,3),
+array(5,1,1,1)
+);
+
+
+?>
