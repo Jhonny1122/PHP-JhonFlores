f38242e1606e4b295d547dc8ed0618fa6ad94416
Jhonny1122 <113983932+Jhonny1122@users.noreply.github.com>
2022-10-11 10:08:16 +0200

    Update ej9am.php

--- ArraysMultidumensionales/ej9am.php
+++ ArraysMultidumensionales/ej9am.php
@@ -1,27 +1,37 @@
 <?php
-// Matriz traspuesta, se cambia las filas a columnas
-/* 
-ejemplo: 
-1	3	4
-
-2	0	2
-
-matriz traspuesta:
-
-1	2
-
-3	0
-
-4	2
 
 
-*/
-// Valores de la Matriz 1
-$matriz1 = array(
-array(2,0,1,5),
-array(3,0,0,3),
-array(5,1,1,1)
-);
+$matriz1 = array(array(1, 2, 3, 4),
+           array(5, 6, 7, 8),
+           array(9, 0, 1, 3));
+  
+  
+$transpuesta = [];
 
+foreach ($matriz1 as $key => $valores){
+	
+    foreach ($valores as $subkey => $subvalores){
+		
+        $transpuesta [$subkey][$key] = $subvalores;
+        
+	}
+}
 
+print_r($transpuesta);
+
+echo "</br>";
+
+echo "</br>";
+
+for ($row = 0; $row < 4; $row++){
+	
+	for ($col = 0; $col < 3; $col++) {
+		
+		echo $transpuesta[$row][$col];
+		
+		echo "  ";
+	}
+	
+	echo "</br>";
+}
 ?>
