e405097b02819464ebe443b2c5000bedd12962fe
Jhonny1122 <113983932+Jhonny1122@users.noreply.github.com>
2022-10-12 13:45:11 +0200

    Add files via upload

---
+++ Formularios/calculadora.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "<h1>Informacion recibida en PHP</h1>";
+
+$num1 = $_GET("operando1");
+
+$num1 = $_GET("operando2");
+
+echo "EL operando 1 es " . $num1;
+
+echo "<br/>";
+
+echo "EL operando 2 es " . $num2;
+
+
+?>
