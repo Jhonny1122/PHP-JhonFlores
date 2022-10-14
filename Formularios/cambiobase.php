b50737ccb56235233ac433bbc50126f06108ad4a
Jhonny1122 <113983932+Jhonny1122@users.noreply.github.com>
2022-10-14 11:08:02 +0200

    Update cambiobase.php

--- Formularios/cambiobase.php
+++ Formularios/cambiobase.php
@@ -1,18 +1,51 @@
 <?php
 
-$decimal = $_POST["decimal"];
+$decimal = limpiar ($_POST["decimal"]);
 
 $base = $_POST["base"];
 
+echo "<h1>Conversor Numerico</h1>";
+
+echo "Numero Decimal: " . $decimal;
+
+echo "<br>";
+
+echo "<br>";
+
+echo convertir ($decimal, $base);
+
 function convertir(int $a , String $b){
 	
-	if ($b == "binario"){
+	if ($b == 2){
 		
-		$binario = decbin($a);
+		return "Numero Binario: " . decbin($a);
+	}	 
+	else if ($b == 8){
 		
-		return "Numero Binario: " . $binario;
+		return "Numero Octal: " . decoct($a);
 	}
-	else if (){}
+	else if ($b == 6){
+		
+		return "Numero Hexadecimal: " . dechex($a);
+	}
+	else if($b == "todos"){
+		
+		return "Numero Binario: " . decbin($a) . "<br>" .
+			   "Numero Octal: " . decoct($a) . "<br>" .
+			   "Numero Hexadecimal: " . dechex($a)
+		;
+	}
+}
+
+function limpiar($data){
+	
+	$data = trim($data);
+	
+	$data = stripslashes($data);
+	
+	$data = htmlspecialchars($data);
+	
+	return $data;
 }
 
 ?>
