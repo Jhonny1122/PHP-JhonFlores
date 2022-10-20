c0f793bb4e67023d5455b27f6b8541a58cb10f95
Jhonny1122 <113983932+Jhonny1122@users.noreply.github.com>
2022-10-20 12:59:34 +0200

    Update fichero3.php

--- Ficheros/fichero3.php
+++ Ficheros/fichero3.php
@@ -4,7 +4,7 @@ $cont = 0;
 
 echo "<h1>Datos de los Alumno</h1>";
 
-echo "<table border=1>";
+echo "<table>";
 
 $fichero = fopen ("alumnos1.txt" , "r");
 // feof es una funcion nos dice si hemos terminado con el fichero.
@@ -16,7 +16,7 @@ while (!feof($fichero)){
 	
 	$linea = fgets($fichero);
 	
-	echo "<tr><td>Alumnos</td><td>";
+	echo "<tr><td>";
 	
 	echo $linea . "</td></tr>";
 }
@@ -25,7 +25,7 @@ echo "</table>";
 
 echo "<br>";
 
-echo "Se han leido $cont lineas";
+echo "Se han leido " . ($cont-1) . " lineas";
 	
 fclose($fichero);
 
