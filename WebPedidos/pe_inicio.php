c708ff506543d51c4a3f67070a2361147004cca4
Jhonny1122 <113983932+Jhonny1122@users.noreply.github.com>
2023-02-08 14:07:52 +0100

    Add files via upload

---
+++ WebPedidos/pe_inicio.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<title>Menu</title>
+	</head>
+	<body>
+		<?php	
+			session_start();
+			
+			if(!isset($_SESSION["usuario"])){
+				
+				header("location:pe_login.php");
+			}
+		?>
+		<h2>Menu de Opciones</h2>
+		<a href="pe_altaped.php">Alta Pedidos</a><br>
+		<a href="pe_consped.php">Consulta Pedidos</a><br>
+		<a href="pe_consprodstock.php">Consulta Producto Stock</a><br>
+		<a href="pe_constock.php">Consulta Productos Stocks </a><br>
+		<a href="pe_topprod.php">Consulta Ventas Productos</a><br>
+		<a href="pe_conspago.php">Consulta Pagos</a><br>
+	</body>
+</html>
